f58cf70cfa848797ca992bde7b7c484168591d9c
Hirusha Fernando <heshanhfernando@gmail.com>
2021-06-13 15:23:45 +0530

    .ass file support added

--- clean.py
+++ clean.py
@@ -1,64 +1,17 @@
 import re
 from typing import Pattern
 from config import MIN_DURATION, MAX_DURATION, KEYWORDS, CREATE_NEW_FILE
+from abc import abstractmethod, ABC
 
 
-class CleanSub:
-
-    def __init__(self, sub_file_path: str):
-        self.__sub_file_path = sub_file_path
-        self.__extracted_sub_content: list[dict] = []
-        self.__extracted_full_content: list[dict] = []
-        self.__unwanted_content = []
-        self.__content_to_write = []
-
-    def __str__(self):
-        pass
-
-    def get_unwanted(self) -> list[dict]:
-        return self.__unwanted_content
-
-    def extract_subtitles(self) -> None:
-        with open(self.__sub_file_path, 'r', encoding='utf8') as sub_file:
-            sub_content: str = sub_file.read()
-            SUB_PATTERN = r'([0-9]+\n.+(\n.+){1,})'
-            REGEX: Pattern[str] = re.compile(SUB_PATTERN)
-            results: list[tuple] = REGEX.findall(sub_content)
-            for result_group in results:
-                lines = result_group[0].split('\n')
-                number = lines[0]
-                timestamp = lines[1]
-                sub = lines[2:]
-                sub_part = {
-                    'number': number,
-                    'timestamp': timestamp,
-                    'content': sub
-                }
-                self.__extracted_sub_content.append(sub_part)
-            self.__extracted_full_content = self.__extracted_sub_content
-
-    def detect_unwanted_by_content(self):
-        # Check content has specific words
-        after_content = []
-        i = 0
-        for content in self.__extracted_sub_content:
-            i += 1
-            sub_content = ' '.join(content['content'])
-            for keyword in KEYWORDS:
-                if keyword in sub_content:
-                    self.__unwanted_content.append(content)
-                    break
-            else:
-                after_content.append(content)
-        self.__extracted_sub_content = after_content
-
-    def _split_timestamp(self, timestamp: str) -> dict:
-        start, end = timestamp.split(' --> ')
-        sh, sm, ss = start.split(':')
-        ss, sms = ss.split(',')
-        eh, em, es = end.split(':')
-        es, ems = es.split(',')
-        return {'start': [int(sh), int(sm), int(ss), int(sms)], 'end': [int(eh), int(em), int(es), int(ems)]}
+class CleanSub(ABC):
+    def __init__(self, sub_file_path: str, filetype: str):
+        self._sub_file_path = sub_file_path
+        self._extracted_sub_content: list[dict] = []
+        self._extracted_full_content: list[dict] = []
+        self._unwanted_content = []
+        self._content_to_write = []
+        self.filetype = filetype
 
     def _calculate_duration(self, start: list[int], end: list[int]) -> float:
         duration = 0.000
@@ -74,37 +27,141 @@ class CleanSub:
         duration = float(f'{duration:.3f}')
         return duration
 
+    def _split_timestamp(self, timestamp: str) -> dict:
+        start, end, sec_separator = '', '', ','
+        if self.filetype == 'srt':
+            start, end = timestamp.split(' --> ')
+        elif self.filetype == 'ass':
+            start, end = timestamp.split(',')
+            sec_separator = '.'
+        sh, sm, ss = start.split(':')
+        ss, sms = ss.split(sec_separator)
+        eh, em, es = end.split(':')
+        es, ems = es.split(sec_separator)
+        return {'start': [int(sh), int(sm), int(ss), int(sms)], 'end': [int(eh), int(em), int(es), int(ems)]}
+
+    def remove_unwanted(self, content_to_remove: list[dict]):
+        for content in self._extracted_full_content:
+            if content in content_to_remove:
+                content_to_remove.remove(content)
+                continue
+            self._content_to_write.append(content)
+
+    def get_unwanted(self) -> list[dict]:
+        return self._unwanted_content
+
+    def detect_unwanted_by_content(self):
+        # Check content has specific words
+        after_content = []
+        i = 0
+        for content in self._extracted_sub_content:
+            i += 1
+            sub_content = ' '.join(content['content'])
+            for keyword in KEYWORDS:
+                if keyword in sub_content:
+                    self._unwanted_content.append(content)
+                    break
+            else:
+                after_content.append(content)
+        self._extracted_sub_content = after_content
+
     def detect_unwanted_by_duration(self):
         after_duration = []
-        for content in self.__extracted_sub_content:
+        for content in self._extracted_sub_content:
             split_timestamp: dict[str, list[int]] = self._split_timestamp(content['timestamp'])
             start = split_timestamp['start']
             end = split_timestamp['end']
             duration = self._calculate_duration(start, end)
             if duration <= MIN_DURATION:
-                self.__unwanted_content.append(content)
+                self._unwanted_content.append(content)
             elif duration >= MAX_DURATION:
-                self.__unwanted_content.append(content)
+                self._unwanted_content.append(content)
             else:
                 after_duration.append(content)
-        self.__extracted_sub_content = after_duration
+        self._extracted_sub_content = after_duration
 
-    def remove_unwanted(self, content_to_remove: list[dict]):
-        for content in self.__extracted_full_content:
-            if content in content_to_remove:
-                content_to_remove.remove(content)
-                continue
-            self.__content_to_write.append(content)
+    @abstractmethod
+    def extract_subtitles(self):
+        pass
+
+    @abstractmethod
+    def create_new_sub_file(self) -> str:
+        pass
+
+
+class CleanSubSRT(CleanSub):
+    def __init__(self, sub_file_path: str):
+        super(CleanSubSRT, self).__init__(sub_file_path, 'srt')
+
+    def extract_subtitles(self) -> None:
+        with open(self._sub_file_path, 'r', encoding='utf8') as sub_file:
+            sub_content: str = sub_file.read()
+            SUB_PATTERN = r'([0-9]+\n.+(\n.+){1,})'
+            REGEX: Pattern[str] = re.compile(SUB_PATTERN)
+            results: list[tuple] = REGEX.findall(sub_content)
+            for result_group in results:
+                lines = result_group[0].split('\n')
+                number = lines[0]
+                timestamp = lines[1]
+                sub = lines[2:]
+                sub_part = {
+                    'number': number,
+                    'timestamp': timestamp,
+                    'content': sub
+                }
+                self._extracted_sub_content.append(sub_part)
+            self._extracted_full_content = self._extracted_sub_content
 
     def create_new_sub_file(self) -> str:
         if CREATE_NEW_FILE:
-            filename = f'{self.__sub_file_path[:-4]}-NEW.{self.__sub_file_path[-3:]}'
+            filename = f'{self._sub_file_path[:-4]}-NEW.{self._sub_file_path[-3:]}'
         else:
-            filename = self.__sub_file_path
+            filename = self._sub_file_path
 
         with open(filename, 'w', encoding='utf8') as sub_file:
-            for i, content in enumerate(self.__content_to_write, 1):
+            for i, content in enumerate(self._content_to_write, 1):
                 sub_content = '\n'.join(content['content'])
                 sub = f"{i}\n{content['timestamp']}\n{sub_content}\n\n"
+                sub_file.write(sub)
+        return filename
+
+
+class CleanSubASS(CleanSub):
+    def __init__(self, sub_file_path: str):
+        super(CleanSubASS, self).__init__(sub_file_path, 'ass')
+        self.info_content: list[str] = []
+
+    def extract_subtitles(self):
+        with open(self._sub_file_path, 'r', encoding='utf8') as sub_file:
+            sub_lines = sub_file.readlines()
+            SUB_PATTERN = r'(D.+: \d,)(\d:\d\d:\d\d\.\d{2,3},\d:\d\d:\d\d\.\d{2,3})(,\w+,,\d,\d,\d,,)(.+)'
+            REGEX: Pattern[str] = re.compile(SUB_PATTERN)
+            results: list[dict] = []
+            for line in sub_lines:
+                if REGEX.match(line):
+                    content = REGEX.findall(line)[0]
+                    sub_part = {
+                        "part_1": content[0],
+                        "timestamp": content[1],
+                        "part_2": content[2],
+                        "content": content[3]
+                    }
+                    self._extracted_sub_content.append(sub_part)
+                else:
+                    self.info_content.append(line)
+            self._extracted_full_content = self._extracted_sub_content
+
+    def create_new_sub_file(self) -> str:
+        if CREATE_NEW_FILE:
+            filename = f'{self._sub_file_path[:-4]}-NEW.{self._sub_file_path[-3:]}'
+        else:
+            filename = self._sub_file_path
+
+        with open(filename, 'w', encoding='utf8') as sub_file:
+            for info in self.info_content:
+                sub_file.write(info)
+
+            for content in self._content_to_write:
+                sub = f"{content['part_1']}{content['timestamp']}{content['part_2']}{content['content']}\n"
                 sub_file.write(sub)
         return filename
