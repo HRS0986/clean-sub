b2dff2deabd7db6d3f7f79cdc00bb5ed00d73009
Hirusha Fernando <heshanhfernando@gmail.com>
2021-06-14 14:16:28 +0530

    Mock data added for testing

---
+++ tests/mock_data.py
@@ -0,0 +1,32 @@
+MOCK_DATA = {
+    "srt_content": {
+
+    },
+    "srt_timestamp": [
+        {"timestamp": "00:00:00,461 --> 00:00:18,865", "content": "LC_1"},
+        {"timestamp": "00:20:04,632 --> 00:20:08,134", "content": "NC_1"},
+        {"timestamp": "00:04:20,671 --> 00:04:20,865", "content": "SC_1"},
+        {"timestamp": "01:44:50,063 --> 01:50:51,361", "content": "LC_2"},
+        {"timestamp": "00:00:01,234 --> 01:25:01,936", "content": "LC_3"},
+        {"timestamp": "02:52:13,465 --> 02:52:13,584", "content": "SC_2"},
+        {"timestamp": "01:41:30,461 --> 01:41:34,865", "content": "NC_2"},
+        {"timestamp": "00:10:50,251 --> 00:10:55,567", "content": "NC_3"},
+        {"timestamp": "01:44:50,063 --> 01:50:50,161", "content": "LC_4"},
+        {"timestamp": "00:00:00,461 --> 01:00:18,665", "content": "LC_5"},
+    ],
+    "ass_content": {
+
+    },
+    "ass_timestamp": {
+        {"timestamp": "00:00:00.36,00:00:18.86", "content": "LC_1"},
+        {"timestamp": "00:20:04.63,00:20:08.13", "content": "NC_1"},
+        {"timestamp": "00:04:20.67,00:04:20.86", "content": "SC_1"},
+        {"timestamp": "01:44:50.06,01:50:51.36", "content": "LC_2"},
+        {"timestamp": "00:00:01.23,01:25:01.93", "content": "LC_3"},
+        {"timestamp": "02:52:13.46,02:52:13.58", "content": "SC_2"},
+        {"timestamp": "01:41:30.46,01:41:34.86", "content": "NC_2"},
+        {"timestamp": "00:10:50.25,00:10:55.56", "content": "NC_3"},
+        {"timestamp": "01:44:50.06,01:50:50.16", "content": "LC_4"},
+        {"timestamp": "00:00:00.46,01:00:18.66", "content": "LC_5"},
+    }
+}
