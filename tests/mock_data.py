39d5d56ef5f19599f375bf94b6718b12e6dda60d
Hirusha Fernando <heshanhfernando@gmail.com>
2021-06-15 05:14:44 +0530

    Updated

--- tests/mock_data.py
+++ tests/mock_data.py
@@ -17,7 +17,7 @@ MOCK_DATA = {
     "ass_content": {
 
     },
-    "ass_timestamp": {
+    "ass_timestamp": [
         {"timestamp": "00:00:00.36,00:00:18.86", "content": "LC_1"},
         {"timestamp": "00:20:04.63,00:20:08.13", "content": "NC_1"},
         {"timestamp": "00:04:20.67,00:04:20.86", "content": "SC_1"},
@@ -28,5 +28,17 @@ MOCK_DATA = {
         {"timestamp": "00:10:50.25,00:10:55.56", "content": "NC_3"},
         {"timestamp": "01:44:50.06,01:50:50.16", "content": "LC_4"},
         {"timestamp": "00:00:00.46,01:00:18.66", "content": "LC_5"},
-    }
+    ],
+    "durations": [
+        {"start": [0, 0, 0, 36], "end": [0, 0, 18, 86], "duration": 18.5},
+        {"start": [0, 20, 4, 63], "end": [0, 20, 8, 13], "duration": 3.5},
+        {"start": [0, 4, 20, 67], "end": [0, 4, 20, 86], "duration": 0.19},
+        {"start": [1, 44, 50, 6], "end": [1, 50, 51, 36], "duration": (6 * 60) + 1.3},
+        {"start": [0, 0, 1, 23], "end": [1, 25, 1, 93], "duration": (25 * 60) + (60 * 60) + 0.7},
+        {"start": [2, 52, 13, 46], "end": [2, 52, 13, 58], "duration": 0.12},
+        {"start": [1, 41, 30, 46], "end": [1, 41, 34, 86], "duration": 4.4},
+        {"start": [0, 10, 50, 25], "end": [0, 10, 55, 56], "duration": 5.31},
+        {"start": [1, 44, 50, 6], "end": [1, 50, 50, 16], "duration": (6 * 60) + 0.1},
+        {"start": [0, 0, 0, 46], "end": [1, 0, 18, 66], "duration": (60 * 60) + 18.2},
+    ]
 }
