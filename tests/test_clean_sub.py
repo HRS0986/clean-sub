9bbbfc3e8bb1a119b8232e49fe9679fcc5228589
Hirusha Fernando <heshanhfernando@gmail.com>
2021-06-15 12:07:00 +0530

    Added some tests

--- tests/test_clean_sub.py
+++ tests/test_clean_sub.py
@@ -1,5 +1,7 @@
 import unittest
 from clean import CleanSubSRT, CleanSubASS
+from mock_data import MOCK_DATA
+from typing import List, Dict
 
 
 class TestCleanSub(unittest.TestCase):
@@ -14,28 +16,42 @@ class TestCleanSub(unittest.TestCase):
         self.assertEqual(ass_filetype, 'ass', msg="Test ASS Filetype")
 
     def test_calculate_duration(self) -> None:
-        start = [00, 1, 29, 41]
-        end = [00, 1, 30, 458]
-        duration: float = self.cleaner_srt._calculate_duration(start, end)
-        self.assertEqual(duration, 1.417, msg="Test Calculate Duration Between Start And End")
+        timelines: List[Dict] = MOCK_DATA['durations']
+        for timeline in timelines:
+            start = timeline['start']
+            end = timeline['end']
+            expected_duration = timeline['duration']
+            duration: float = self.cleaner_srt._calculate_duration(start, end)
+            self.assertEqual(expected_duration, duration, msg="Test Calculate Duration Between Start And End")
 
     def test_split_timeline_srt(self) -> None:
         timestamp = "00:01:29,041 --> 00:01:30,458"
         after_split = {
-            'start': [0, 1, 29, 41],
-            'end': [0, 1, 30, 458]
+            'start': [0, 1, 29.041],
+            'end': [0, 1, 30.458]
         }
-        self.assertEqual(self.cleaner_srt._split_timestamp(timestamp), after_split, msg="Test Timestamp Split Of A SRT File")
+        self.assertEqual(after_split, self.cleaner_srt._split_timestamp(timestamp), msg="Test Timestamp Split Of A SRT File")
 
     def test_split_timeline_ass(self) -> None:
         timestamp = "0:00:01.63,0:00:18.22"
         after_split = {
-            'start': [0, 0, 1, 63],
-            'end': [0, 0, 18, 22]
+            'start': [0, 0, 1.63],
+            'end': [0, 0, 18.22]
         }
         self.assertEqual(self.cleaner_ass._split_timestamp(timestamp), after_split, msg="Test Timestamp Split Of A ASS File")
 
-    # def
+    def test_detect_by_duration(self) -> None:
+        self.cleaner_srt._extracted_sub_content = MOCK_DATA['srt_timestamp']
+        self.cleaner_srt.detect_unwanted_by_duration()
+        expected_content = MOCK_DATA['srt_timestamp_expected_duration']
+        self.assertEqual(expected_content, self.cleaner_srt._extracted_sub_content, msg="Test Detect By Display Duration Of A Subtitle")
+
+    def test_remove_unwanted(self) -> None:
+        expected_content = MOCK_DATA['srt_content_to_write']
+        to_remove = MOCK_DATA['srt_timestamp_expected_duration']
+        self._extracted_full_content = MOCK_DATA['srt_content']
+        self.cleaner_srt.remove_unwanted(to_remove)
+        self.assertEqual(expected_content, self.cleaner_srt._content_to_write, msg="Test Remove Unwanted Content")
 
 
 if __name__ == '__main__':
