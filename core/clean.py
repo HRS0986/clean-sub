64b62089b247798a2db32d85fe513e614f96c64b
Hirusha Fernando <heshanhfernando@gmail.com>
2021-07-06 20:35:29 +0530

    CleanSubSmi Class Created

--- core/clean.py
+++ core/clean.py
@@ -2,7 +2,8 @@ import re
 from .config import MIN_DURATION, MAX_DURATION, KEYWORDS, CREATE_NEW_FILE, REMOVE_EMPTY
 from abc import abstractmethod, ABC
 from typing import List, Union, Pattern, Dict
-from .dtypes import ContentList, SplitTimestamp, SRTSubPart, ASSSubPart, SRTRegexResults, ASSRegexResults
+from .dtypes import ContentList, SplitTimestamp, SRTSubPart, ASSSubPart, SMISubPart
+from .dtypes import SRTRegexResults, ASSRegexResults, SMIRegexResults
 
 
 class CleanSub(ABC):
@@ -14,7 +15,7 @@ class CleanSub(ABC):
         self._content_to_write: ContentList = []
         self.filetype = filetype
         self.__CONTENT_PTN = content_ptn
-        self.__CONTENT_REGEX: Pattern[str] = re.compile(self.__CONTENT_PTN)
+        self._CONTENT_REGEX: Pattern[str] = re.compile(self.__CONTENT_PTN)
 
     def _calculate_duration(self, start: List[int], end: List[int]) -> float:
         duration = 0.000
@@ -42,6 +43,17 @@ class CleanSub(ABC):
         elif self.filetype == 'ass':
             start, end = timestamp.split(',')
             sec_separator = '.'
+        elif self.filetype == 'smi':
+            start, end = timestamp.split('-')
+            sms, ems = int(start[-3:]), int(end[-3:])
+            start, end = int(start[:-3]), int(end[:-3])
+            sh, eh = start / 3600, end / 3600
+            start, end = start % 3600, end % 3600
+            sm, em = start / 60, end / 60
+            ss, es = start % 60, end % 60
+            s_sec = float(f"{ss}.{sms}")
+            e_sec = float(f"{es}.{ems}")
+            return {'start': [sh, sm, s_sec], 'end': [eh, em, e_sec]}
 
         sh, sm, ss = start.split(':')
         ss, sms = ss.split(sec_separator)
@@ -106,7 +118,7 @@ class CleanSubSRT(CleanSub):
     def extract_subtitles(self) -> None:
         with open(self._sub_file_path, 'r', encoding='utf8') as sub_file:
             sub_content: str = sub_file.read()
-            results: SRTRegexResults = self.__CONTENT_REGEX.findall(sub_content)
+            results: SRTRegexResults = self._CONTENT_REGEX.findall(sub_content)
             for result_group in results:
                 lines: List[str] = result_group[0].split('\n')
                 number: str = lines[0]
@@ -150,8 +162,8 @@ class CleanSubASS(CleanSub):
         with open(self._sub_file_path, 'r', encoding='utf8') as sub_file:
             sub_lines = sub_file.readlines()
             for line in sub_lines:
-                if self.__CONTENT_REGEX.match(line):
-                    content: ASSRegexResults = self.__CONTENT_REGEX.findall(line)[0]
+                if self._CONTENT_REGEX.match(line):
+                    content: ASSRegexResults = self._CONTENT_REGEX.findall(line)[0]
                     sub_part: ASSSubPart = {
                         "part_1": content[0],
                         "timestamp": content[1],
@@ -204,3 +216,36 @@ class CleanSubASS(CleanSub):
             else:
                 break
         self._info_content = info
+
+
+class CleanSubSmi(CleanSub):
+    def __init__(self, sub_file_path: str):
+        SMI_CONTENT_PTN = r"(<SYNC.+)\n(.+)\n(<SYNC .+?nbsp)"
+        super(CleanSubSmi, self).__init__(sub_file_path, 'smi', SMI_CONTENT_PTN)
+        self._info_content: Dict[str, str] = {"head": "", "tale": "\n</BODY>\n</SAMI>"}
+        self.__INFO_HEAD_PTN = r"<SAMI>.+<BODY>\n"
+        self.__TIMESTAMP_PTN = r"Start=(\d+?)>"
+        self.__INFO_HEAD_REGEX = re.compile(self.__INFO_HEAD_PTN, flags=re.DOTALL)
+        self.__TIMESTAMP_REGEX = re.compile(self.__TIMESTAMP_PTN)
+
+    def extract_subtitles(self):
+        with open(self._sub_file_path, 'r', encoding='utf16', errors='ignore') as sub_file:
+            sub_content: str = sub_file.read()
+            content_results: SMIRegexResults = self._CONTENT_REGEX.findall(sub_content)
+            for result_group in content_results:
+                start = self.__TIMESTAMP_REGEX.findall(result_group[0])[0]
+                end = self.__TIMESTAMP_REGEX.findall(result_group[2])[0]
+                sub_part: SMISubPart = {
+                    "start": result_group[0],
+                    "end": result_group[2],
+                    "timestamp": f"{start}-{end}",
+                    "content": result_group[1]
+                }
+                self._extracted_sub_content.append(sub_part)
+            self._extracted_full_content = self._extracted_sub_content
+
+            info_results: str = self.__INFO_HEAD_REGEX.findall(sub_content)[0]
+            self._info_content = info_results
+
+    def create_new_sub_file(self) -> str:
+        pass
