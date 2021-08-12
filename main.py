6168d2b0aea77c4ff233a37a8c1c2f2e60e2b21d
Hirusha Fernando <heshanhfernando@gmail.com>
2021-08-12 07:40:47 +0530

    Added doc stings

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from config.default_config import FILE_TYPES
 
 
 class PathValidator(Validator):
+    """
+    Validator for validate user entered subtitle file's path
+    """
     def validate(self, document):
         valid_path: bool = os.path.isfile(document.text)
         valid_type: bool = document.text[-3:] in FILE_TYPES
@@ -28,13 +31,16 @@ class PathValidator(Validator):
 
 
 def take_sub_file_from_user() -> Tuple[str, str]:
+    """
+    Ask subtitle files location from user
+    :return: subtitle file's path and filetype
+    """
     question_1 = {
         "type": "input",
         "name": "sub_file_path",
         "message": "Enter Subtitle File Path: ",
         "validate": PathValidator
     }
-
     answer_1: dict = prompt(question_1)
     sub_file_path: str = answer_1['sub_file_path'].strip('"')
     sub_file_path = sub_file_path.strip("' ")
@@ -47,15 +53,18 @@ if __name__ == '__main__':
         description="Remove Annoying Stuff From Subtitle Files",
         prog="cleansub",
         epilog="By Hirusha Fernando",
-        usage="%(prog)s [-h] [-f FILE] [-m MAX] [-n MIN] [-e | -g] [-c | -x] [-k KEYWORDS | -a KEYWORDS -r KEYWORDS] [-s SCRIPT NAME]"
+        usage="%(prog)s [-h] [-f FILE] [-m MAX] [-n MIN] [-e | -g] [-c | -x] [-k KEYWORDS | -a KEYWORDS -r KEYWORDS] [-s SCRIPT_SAVE NAME | -u SCRIPT_NAME]"
     )
-    parser.add_argument('-s', '--save', help="Save configuration to script file", type=str, dest="SCRIPT_NAME")
     parser.add_argument('-m', '--max', help="Maximum subtitle display duration to check", type=int, dest="MAX")
     parser.add_argument('-n', '--min', help="Minimum subtitle display duration to check", type=int, dest="MIN")
     parser.add_argument('-f', '--file', help="Subtitle file path", type=Path, dest="FILE")
     parser.add_argument('-k', '--keywords', help="Keywords checklist (Comma Separated)", type=str, dest="K", nargs="+")
     parser.add_argument('-r', '--exc', help="Keywords to exclude from default list (Comma Separated)", type=str, dest="EX_K", nargs="+")
     parser.add_argument('-a', '--add', help="Keywords to add default list (Comma Separated)", type=str, dest="ADD_K", nargs="+")
+
+    group_script = parser.add_mutually_exclusive_group()
+    group_script.add_argument('-s', '--save', help="Save configuration to script file", type=str, dest="SCRIPT_SAVE_NAME")
+    group_script.add_argument('-u', '--use', help="Use another configuration script file", type=str, dest="SCRIPT_NAME")
 
     group_empty = parser.add_mutually_exclusive_group()
     group_empty.add_argument('-g', '--keep-empty', help="Keep empty content lines", action="store_true")
@@ -88,6 +97,7 @@ if __name__ == '__main__':
         args.keep_empty,
         args.MIN,
         args.MAX,
+        args.SCRIPT_SAVE_NAME,
         args.SCRIPT_NAME,
         args.new,
         args.ext,
